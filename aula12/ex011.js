968ceb8a1bb923e1b1ce19cf9df2031324ec1845
Felipe <143225058+Felipe572@users.noreply.github.com>
2024-03-07 20:05:39 -0300

    Condição Aninhadas

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 77
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade >= 16 && idade < 18 || idade > 65){
+        console.log('Voto opcional')
+    } else {
+        console.log('Voto obrigatório')
+    }
+
+console.log('Vote conciente!')
+console.log('Boca de urna e crime, Denuncie caso veja!') 
